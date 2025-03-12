2511a73edcc93a100b51f53ca9a7d7c6af63e1fa
Avee1000 <eddyidahosa01@gmail.com>
2025-03-12 10:53:05 +0100

    Replace Font Awesome icons with SVGs in navigation and enhance menu functionality for improved user experience

--- chamber/scripts/hamburger.js
+++ chamber/scripts/hamburger.js
@@ -2,5 +2,11 @@ const hamburger = document.querySelector("#menu");
 const navigation = document.querySelector("nav");
 
 hamburger.addEventListener('click', () => {
-    navigation.classList.toggle('show');
+    navigation.classList.add('show');
+    document.body.style.overflow = 'hidden';
+});
+
+document.querySelector("#close").addEventListener('click', () => {
+    navigation.classList.remove('show');
+    document.body.style.overflow = '';
 });
