29ee659782dccbb5c9b74ca9e1dcc72da7dcfb01
Avee1000 <ida23001@byupathway.edu>
2025-03-24 15:21:38 +0100

    Refactor right-side section styles and add animation for improved visual appeal and user interaction

--- chamber/scripts/hamburger.js
+++ chamber/scripts/hamburger.js
@@ -10,3 +10,27 @@ document.querySelector("#close").addEventListener('click', () => {
     navigation.classList.remove('show');
     document.body.style.overflow = '';
 });
+
+////////////////////////////////////////////////////////////
+const rightSideSections = document.querySelectorAll("#right-side section");
+
+rightSideSections.forEach((section, index) => {
+    section.style.animationDelay = `${index * 0.1}s`; // 0.5s delay between each section
+});
+
+const learnMore = document.querySelectorAll(".learnMore");
+
+learnMore.forEach((button) => {
+    button.addEventListener("click", () => {
+        const section = button.parentElement;  //or use .closest(); or previousElementSibling;
+        const listContainer = section.querySelector(".listContainer"); 
+        listContainer.classList.toggle("show");
+
+        if (listContainer.classList.contains("show")) {
+            button.classList.add("show");
+        } 
+        else {
+            button.classList.remove("show");
+        }
+    });
+});
